b4c2ba85dd7583c9941e3cb942b25ba576ee2e4b
Jacky Setiawan <jackysetiawan6@gmail.com>
2023-10-06 13:47:01 +0700

    Updated.

--- Example_C_C++.cpp
+++ Example_C_C++.cpp
@@ -1,61 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <limits.h>
 #define min(a, b) (a < b ? a : b)
 #define max(a, b) (a > b ? a : b)
 
 int DP[1000000] = {};
 int DP_2D[1000][1000] = {};
 
-int is_palindrome(char string[], int LEFT, int RIGHT)
+void soal_01()
 {
-    while (LEFT < RIGHT)
+    char input[100] = ""; scanf("%[^\n]", input);
+    int length = strlen(input);
+    for (int i = 0; i < length; i++)
     {
-        if (string[LEFT++] != string[RIGHT--])
-        {
-            return 0;
-        }
+        if (input[i] >= 'a') input[i] -= 32;
+        else if (input[i] >= 'A') input[i] += 32;
     }
-    return 1;
+    printf("%s\n", input);
+    return;
 }
 
-int is_unique(char string[], int start, int end)
+void soal_02()
 {
-    int index = 0, first = string[start] - 97;
-    for (int i = start; i <= end; i++)
+    int number = 0, kesabaran = 0, input[1010] = {}, sum = 0, count = 0;
+    scanf("%d %d", &number, &kesabaran);
+    for (int i = 0; i < number; i++)
     {
-        index = index + ((string[i] - 96) << 1);
+        scanf("%d", &input[i]);
+        sum += input[i];
     }
-    if (DP_2D[first][index] == 1)
+    int durasi = (int)(sum / number * kesabaran / 100);
+    for (int i = 0; i < number; i++)
     {
-        return 0;
-    }
-    printf("%02d --- %02d\n", first, index);
-    DP_2D[first][index] = 1;
-    return 1;
-}
-
-int print_substring(char STRING[])
-{
-    int COUNT = 0;
-    int LENGTH = (int)strlen(STRING);
-    for (int i = 1; i <= LENGTH; i++)
-    {
-        for (int j = 0; j < LENGTH - i + 1; j++)
+        if (input[i] > durasi)
         {
-            if (is_palindrome(STRING, j, j + i - 1))
-            {
-                if (is_unique(STRING, j, j + i - 1)) COUNT++;
-            }
+            printf("%s", count == 0 ? "" : " ");
+            printf("%d", i + 1);
+            count++;
         }
     }
-    return COUNT;
+    if (count == 0) printf("0");
+    return;
+}
+
+void soal_03()
+{
+    int apel = 0, jeruk = 0, nanas = 0;
+    scanf("%d %d %d", &apel, &jeruk, &nanas);
+    int harga_apel = apel * 5000;
+    int harga_jeruk = jeruk * 6000;
+    int harga_nanas = nanas * 7000;
+    if (harga_apel > harga_jeruk && harga_apel > harga_nanas) printf("%d Apel\n", harga_apel);
+    else if (harga_jeruk > harga_apel && harga_jeruk > harga_nanas) printf("%d Jeruk\n", harga_jeruk);
+    else if (harga_nanas > harga_apel && harga_nanas > harga_jeruk) printf("%d Nanas\n", harga_nanas);
+    else printf("Cancel\n");
+    return;
+}
+
+void soal_04()
+{
+	char input[100] = ""; scanf("%[^\n]", input);
+    int length = strlen(input);
+    int i = 0, j = 0;
+    while (i < length)
+    {
+        j = i;
+        while (j < length && input[j] != ' ') j++;
+        for (int k = i; k < (i + j) / 2; k++)
+        {
+            char temp = input[k];
+            input[k] = input[i + j - k - 1];
+            input[i + j - k - 1] = temp;
+        }
+        i = j + 1;
+    }
+    printf("%s\n", input);
+    return;
+}
+
+void soal_05()
+{
+    int ban = 0, pintu = 0, kaca = 0;
+    scanf("%d %d %d", &ban, &pintu, &kaca);
+    int min_ban = 4, min_pintu = 2, min_kaca = 1;
+    int number_ban = ceil((double)ban / min_ban);
+    int number_pintu = ceil((double)pintu / min_pintu);
+    int number_kaca = ceil((double)kaca / min_kaca);
+    int number = max(number_ban, max(number_pintu, number_kaca));
+    int required_ban = number * min_ban - ban;
+    int required_pintu = number * min_pintu - pintu;
+    int required_kaca = number * min_kaca - kaca;
+    printf("%d %d %d %d\n", number, required_ban, required_pintu, required_kaca);
+    return;
 }
 
 int main(int argc, char *argv[])
 {
-    char kata[1000] = "";
-    scanf("%[^\n]", kata);
-    printf("%d\n", print_substring(kata));
+    soal_04();
     return 0;
 }
