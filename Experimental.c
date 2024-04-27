c1606f3cdbce3fca5ad72d107cb6bfa36fd5fb4b
Jacky Setiawan <jackysetiawan6@gmail.com>
2024-04-27 21:57:50 +0700

    Minor update

--- Experimental.c
+++ Experimental.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *a, int *b);
+void printer(int *arr, int size);
 
 int main()
 {
-    printf("Hello World!\n");
+    // s
     return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+void printer(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        printf("%d%c", arr[i], i == size - 1 ? '\n' : ' ');
+    return;
 }
